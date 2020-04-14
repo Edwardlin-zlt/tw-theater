c6e32e43a87d1a59c5083a12b1ba8d51cab01839
Edwardlin-zlt <edwardlin.zlt@gmail.com>
2020-04-14 10:32:20 +0800

    [Edward] {feat}: init repo

---
+++ src/test/java/com/thoughtworks/tw_theater/TwTheaterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.tw_theater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwTheaterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
