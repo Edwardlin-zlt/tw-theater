95ea72c1e1340c456e0e04c7b10932841b710449
Edwardlin-zlt <edwardlin.zlt@gmail.com>
2020-04-14 11:36:27 +0800

    [Edward] {feat}: file structure

---
+++ src/main/java/com/thoughtworks/tw_theater/MoviesService.java
@@ -0,0 +1,7 @@
+package com.thoughtworks.tw_theater;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MoviesService {
+}
---
+++ src/main/java/com/thoughtworks/tw_theater/TheaterController.java
@@ -0,0 +1,7 @@
+package com.thoughtworks.tw_theater;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class TheaterController {
+}
